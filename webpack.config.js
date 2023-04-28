5fe2637d96aa4a7ecf3429ca6903ca37bd232437
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-04-28 16:16:56 +0300

    update project for image

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,8 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js',
+		clean: true,
+		assetModuleFilename: 'assets/[name][ext]',
 	},
 	module: {
 		rules: [
@@ -23,10 +25,35 @@ module.exports = {
 				test: /\.(c|sa|sc)ss$/i,
 				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
 			},
-			// {
-			// 	test: /\.(png|svg|jpg|jpeg|gif)$/i,
-			// 	type: 'assets/resource',
-			// },
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+				use: [
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							mozjpeg: {
+								progressive: true,
+							},
+							// optipng.enabled: false will disable optipng
+							optipng: {
+								enabled: false,
+							},
+							pngquant: {
+								quality: [0.65, 0.9],
+								speed: 4,
+							},
+							gifsicle: {
+								interlaced: false,
+							},
+							// the webp option will enable WEBP
+							webp: {
+								quality: 75,
+							},
+						},
+					},
+				],
+			},
 		],
 	},
 	plugins: [
